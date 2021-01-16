daae6566d8faf319d5d4abd70f253d57f5aea102
v1pu2 <v1bhalodia@gmail.com>
2021-01-16 21:28:54 +0530

    set ui of detail with data

--- src/containers/Detail.js
+++ src/containers/Detail.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import {
   SafeAreaView,
   StyleSheet,
@@ -11,8 +11,8 @@ import {
   TouchableOpacity,
 } from "react-native";
 import AppImages from "../theme/AppImages";
-import VideoPlayer from 'react-native-video-player'
-
+import VideoPlayer from "react-native-video-player";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 const list = [
   { id: 1, name: "Salt" },
   { id: 2, name: "Papper" },
@@ -30,101 +30,158 @@ const steps = [
   { id: 6, name: "step6", isImage: false },
   { id: 7, name: "Sstep1alt", isImage: true },
 ];
-const Detail = (props) => {
-  return (
-    <ImageBackground
-      source={AppImages.mainBack}
-      style={styles.imgBg}
-      resizeMode='stretch'
-    >
-      <View style={styles.videoView}>
-        <View style={styles.topRowView}>
-          <Text>arrow</Text>
-          <Text>share</Text>
-        </View>
-        <View style={styles.playerView}>
-          <VideoPlayer
-            video={{
-              uri:
-                "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
-            }}
-            videoWidth={1600}
-            videoHeight={900}
-            thumbnail={{ uri: "https://i.picsum.photos/id/866/1600/900.jpg" }}
-          />
-          {/* <Image source={AppImages.heartInActive} style={styles.ImageStyle} /> */}
-        </View>
-      </View>
-
-      <View style={styles.detailView}>
-        <View style={styles.imgView}>
-          <Image source={AppImages.heartInActive} style={styles.ImageStyle} />
-          <Text>10.5k</Text>
-        </View>
-        <View style={styles.serviceView}>
-          <Text style={styles.txtService}>Searving 4</Text>
-        </View>
-        <Text style={styles.txtCommon}>title</Text>
-        <Text style={styles.txtCommon}>
-          Description Description Description Description Description
-          Description
-        </Text>
-        <View style={{ flexDirection: "row", margin: 5 }}>
-          <Text>Ingredients</Text>
-          <Text> (6)</Text>
-        </View>
-        <View style={styles.rowView}>
-          {list &&
-            list.map((element, i) => {
-              return (
-                <View key={element.id}>
-                  <View style={styles.tabView}>
-                    <Text style={styles.txtTab} textBreakStrategy={"simple"}>
-                      {element && element.name}
-                    </Text>
-                  </View>
-                </View>
-              );
-            })}
-        </View>
-        <Text style={styles.txtCommon}>Steps</Text>
-        <ScrollView>
-          <View>
-            {steps &&
-              steps.map((item, i) => {
-                return (
-                  <View style={styles.stepInnerView} key={item.id}>
-                    <View style={styles.indexView}>
-                      <Text>{item.id}</Text>
-                    </View>
-                    <View>
-                      <Text>{item.name}</Text>
-                      {item.isImage && (
-                        <Image
-                          source={AppImages.heartInActive}
-                          style={styles.ImageStyle}
-                        />
-                      )}
-                    </View>
-                  </View>
-                );
-              })}
+class Detail extends Component {
+  constructor(props) {
+    // console.log('in s=const',props.navigation.state.params)
+    super(props);
+    this.state = {
+      data: {},
+    };
+  }
+  async componentDidMount() {
+    // console.log('tis.state.id'.this.state.id1)
+    const token = await AsyncStorage.getItem("TOKEN");
+    const id = await AsyncStorage.getItem("RECIPEID");
+    console.log("in didount", id);
+    const fullToken = `Bearer ${token}`;
+    console.log("fulltoken", fullToken);
+    // if(id && token){
+    fetch("https://webapps.iqlance-demo.com/chef/public/api/recipeDetail", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        accept: "application/json",
+        Authorization: fullToken,
+      },
+      body: JSON.stringify({
+        recipesID: id,
+      }),
+    })
+      .then((response) => response.json())
+      .then((response) => {
+        console.log("resonse in detail", response);
+        this.setState({ data: response && response.data });
+      })
+      .catch((err) => {
+        console.log("error", err);
+      });
+    // }
+  }
+  render() {
+    const { data } = this.state;
+    console.log("in render", this.state.data);
+    const recipes = data && data.recipes;
+    const ingres = data && data.recipeIngredients;
+    const steps = data && data.steps;
+    return (
+      <ImageBackground
+        source={
+          recipes && recipes.image ? { uri: recipes.image } : AppImages.mainBack
+        }
+        style={styles.imgBg}
+        resizeMode='stretch'
+      >
+        <View style={styles.videoView}>
+          <View style={styles.topRowView}>
+            <Text>arrow</Text>
+            <Text>share</Text>
           </View>
-        </ScrollView>
-      </View>
-    </ImageBackground>
-  );
-};
+          {recipes && recipes.video !== "" && (
+            <View style={styles.playerView}>
+              <VideoPlayer
+                video={{
+                  uri:
+                    "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+                }}
+                videoWidth={1600}
+                videoHeight={900}
+                thumbnail={{
+                  uri: "https://i.picsum.photos/id/866/1600/900.jpg",
+                }}
+              />
+              {/* <Image source={AppImages.heartInActive} style={styles.ImageStyle} /> */}
+            </View>
+          )}
+        </View>
+
+        <View style={styles.detailView}>
+          <View style={styles.imgView}>
+            <Image source={AppImages.heartInActive} style={styles.ImageStyle} />
+            <Text>{recipes && recipes.favStatus}</Text>
+          </View>
+          <View style={styles.serviceView}>
+            <Text style={styles.txtService}>
+              Searving {recipes && recipes.favorite_recipe}
+            </Text>
+          </View>
+          <Text style={styles.txtCommon}>{recipes && recipes.name}</Text>
+          <Text style={styles.txtCommon}>{recipes && recipes.description}</Text>
+          {ingres && ingres.length > 0 && (
+            <View>
+              <View style={{ flexDirection: "row", margin: 5 }}>
+                <Text>Ingredients </Text>
+                <Text>({ingres && ingres.length})</Text>
+              </View>
+              <View style={styles.rowView}>
+                {ingres &&
+                  ingres.map((element, i) => {
+                    return (
+                      <View key={element.id}>
+                        <View style={styles.tabView}>
+                          <Text
+                            style={styles.txtTab}
+                            textBreakStrategy={"simple"}
+                          >
+                            {element && element.name}
+                          </Text>
+                        </View>
+                      </View>
+                    );
+                  })}
+              </View>
+            </View>
+          )}
+          {steps && steps.length > 0 && (
+            <Text style={styles.txtCommon}>Steps</Text>
+          )}
+          <ScrollView>
+            <View>
+              {steps &&
+                steps.map((item, i) => {
+                  return (
+                    <View style={styles.stepInnerView} key={item.id}>
+                      <View style={styles.indexView}>
+                        <Text>{item.stepID}</Text>
+                      </View>
+                      <View>
+                        <Text>{item.title}</Text>
+                        {item.image && (
+                          <Image
+                            source={{ uri: item && item.image }}
+                            style={styles.ItemImageStyle}
+                          />
+                        )}
+                      </View>
+                    </View>
+                  );
+                })}
+            </View>
+          </ScrollView>
+        </View>
+      </ImageBackground>
+    );
+  }
+}
 export default Detail;
 const styles = StyleSheet.create({
   imgBg: { height: "100%", width: "100%" },
   videoView: {
     flex: 0.8,
   },
-  txtCommon:{
-    fontSize:14,
-    
-    padding:5
+  txtCommon: {
+    fontSize: 14,
+
+    padding: 5,
   },
   topRowView: {
     flexDirection: "row",
@@ -145,6 +202,14 @@ const styles = StyleSheet.create({
     margin: 5,
     height: 35,
     width: 35,
+    resizeMode: "stretch",
+    alignItems: "center",
+  },
+  ItemImageStyle: {
+    padding: 10,
+    margin: 5,
+    height: 100,
+    width: 200,
     resizeMode: "stretch",
     alignItems: "center",
   },
