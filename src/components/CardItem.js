5185f45abb9a4ff9ff6be7110253f072f1471386
v1pu2 <v1bhalodia@gmail.com>
2021-01-16 16:22:38 +0530

    create card item set and horizontal scrollview

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -1,11 +1,82 @@
 import React from "react";
-import { View,Text } from "react-native";
+import {
+  View,
+  StyleSheet,
+  ImageBackground,
+  ActivityIndicator,
+} from "react-native";
+import { Image, Card, Text, Avatar } from "react-native-elements";
+import AppImages from "../theme/AppImages";
 
-const CardItem = () => {
+const CardItem = (props) => {
+ 
   return (
-    <View>
-      <Text>CardItem</Text>
-    </View>
+    <Card containerStyle={styles.card}>
+      <ImageBackground
+        source={AppImages.mainBack}
+        style={styles.imgBg}
+        borderRadius={10}
+        resizeMode='stretch'
+      >
+        <View style={styles.imgView}>
+          <Image source={AppImages.heartInActive} style={styles.ImageStyle} />
+          <Text >10.5k</Text>
+        </View>
+        <View style={styles.avatarView}>
+          <Avatar rounded source={AppImages.heartActive} size='large' />
+        </View>
+        <View style={styles.textContainer}>
+          <Text style={styles.companyName}>company name</Text>
+          <Text style={styles.text}>description fsfks sdfhsf sdfsf kfsjf </Text>
+        </View>
+      </ImageBackground>
+    </Card>
   );
 };
 export default CardItem;
+
+const styles = StyleSheet.create({
+  card: {
+    padding: -5,
+    marginLeft: 0,
+    height: 250,
+    width: "100%",
+    borderRadius: 10,
+    elevation: 5,
+    justifyContent: "center",
+    alignItems: "center",
+    alignContent: "center",
+    borderColor: "#fff",
+    position: "relative",
+  },
+  imgBg: { height: "100%", width: '100%'},
+  ImageStyle: {
+    padding: 10,
+    margin: 5,
+    height: 25,
+    width: 25,
+    resizeMode: "stretch",
+    alignItems: "center",
+  },
+  imgView: { alignItems: "flex-end" ,paddingEnd:10},
+  avatarView: { justifyContent: "center", alignItems: "center", padding: 10 },
+  textContainer: {
+    flex: 1,
+    alignItems: "flex-start",
+    justifyContent: "flex-end",
+    alignContent: "center",
+    padding: 10,
+  },
+  text: {
+    color: "#ababab",
+    fontSize: 14,
+    textTransform: "capitalize",
+    overflow: "hidden",
+  },
+
+  companyName: {
+    fontSize: 16,
+
+    overflow: "hidden",
+  },
+});
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,30 +1,77 @@
 import React, { Component } from "react";
-import { View, StyleSheet, Text, SafeAreaView } from "react-native";
+import { View, StyleSheet, Text, SafeAreaView, ScrollView,TouchableHighlight } from "react-native";
 import { Avatar } from "react-native-elements";
-import AppImages from '../theme/AppImages'
+import AppImages from "../theme/AppImages";
+import CardItem from "../components/CardItem";
 class Home extends Component {
+    constructor(props){
+        super(props);
+        this.state={}
+    }
   render() {
     return (
       <SafeAreaView style={styles.container}>
         <View style={styles.topRoView}>
-        <View>
-          <Text>Hello</Text>
-          <Text>name</Text>
-        </View>
+          <View>
+            <Text>Hello</Text>
+            <Text>name</Text>
+          </View>
           <Avatar rounded source={AppImages.heartActive} size='medium' />
-      </View>
+        </View>
+        <ScrollView showsHorizontalScrollIndicator={false}>
+          <View style={styles.rowView}>
+            <Text style={styles.txtPopular}>Most Popular Recipes</Text>
+            <Text style={styles.txtViewall}>View All</Text>
+          </View>
+
+          <View>
+            <ScrollView
+              horizontal
+              contentContainerStyle={styles.scrollHori}
+              showsHorizontalScrollIndicator={false}
+            >
+              <TouchableHighlight
+                onPress={() => this.props.navigation.navigate("Detail")}
+                style={styles.upperRowView}
+              >
+                <CardItem />
+              </TouchableHighlight>
+              <TouchableHighlight
+                onPress={() => this.props.navigation.navigate("Detail")}
+                style={styles.upperRowView}
+              >
+                <CardItem />
+              </TouchableHighlight>
+              <TouchableHighlight
+                onPress={() => this.props.navigation.navigate("Detail")}
+                style={styles.upperRowView}
+              >
+                <CardItem />
+              </TouchableHighlight>
+            </ScrollView>
+          </View>
+        </ScrollView>
       </SafeAreaView>
     );
   }
 }
 export default Home;
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-    },
-    topRoView: {
-        flexDirection: "row",
-        padding: 20,
-        justifyContent: "space-between",
-      },
+  container: {
+    flex: 1,
+    padding: 15,
+  },
+  topRoView: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  upperRowView: { width: 200, marginRight: 10 },
+  rowView: {
+    flexDirection: "row",
+    paddingTop: 15,
+    justifyContent: "space-between",
+  },
+  txtPopular: { fontSize: 15, fontFamily: "OpenSans-SemiBold" },
+  txtViewall: { fontSize: 15, fontFamily: "OpenSans-SemiBold", color: "red" },
+  scrollHori: { paddingTop: 20,},
 });
