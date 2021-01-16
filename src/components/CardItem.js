fe875fc00d471c89ac2128a65410c91287547f78
v1pu2 <v1bhalodia@gmail.com>
2021-01-16 14:54:51 +0530

    set common card view

---
+++ src/components/CardItem.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { View } from "react-native";
+
+const CardItem = () => {
+  return (
+    <View>
+      <Txt>CardItem</Txt>
+    </View>
+  );
+};
+export default CardItem;
